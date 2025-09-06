64d5fcfd6af318af4608a1e09a68864d8c462ad2
yordan-gergov01 <yordansoftskills@gmail.com>
2025-09-06 16:16:02 +0300

    setting up knex config for database

---
+++ backend/src/config/db.ts
@@ -0,0 +1,15 @@
+import knex from "knex";
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  // will turn on debugging for all queries
+  //   debug: true,
+});
+
+export default db;
